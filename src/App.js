bf2cf074a4699956f96b30b4fd994776521c13f1
yigittin <i.yigittin@hotmail.com>
2021-07-05 01:10:07 +0300

    Adding Item

--- src/App.js
+++ src/App.js
@@ -11,7 +11,18 @@ function App() {
 
   const handleSubmit=(e)=>{
     e.preventDefault();
-    console.log('hello')
+    if(!name){
+      //display alert
+    }
+    else if(name && isEditing){
+      //edit
+    }
+    else{
+      //show alert
+      const newItem={id:new Date().getTime().toString(),title:name}
+      setList([...list,newItem]);
+      setName('')
+    }
   }
 
   return (
@@ -20,16 +31,20 @@ function App() {
         {alet.show&&<Alert/>}
         <h3>grocery bud</h3>
         <div className="form-control">
-          <input type="text" />
+          <input type="text" className='grocery' placeholder='e.g. eggs'
+          value={name} onChange={(e)=> setName(e.target.value)} />
           <button type='submit' className='submit-btn'>
             {isEditing? 'edit':'submit'}
           </button>
         </div>
       </form>
-    <div className="grocery-container">
-      <List/>
+      {list.length>0&&
+      <div className="grocery-container">
+      <List items={list}/>
       <button className='clear-btn'>clear items</button>
     </div>
+      }
+    
     </section>
     )
 }
