a0d3af4ee3188f4acf6fcf0e8cbcf64a0130d4df
yigittin <i.yigittin@hotmail.com>
2021-07-07 22:41:45 +0300

    ToDo App Finish (Local Storage - Edit)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage=()=>{
+  let list=localStorage.getItem('list')
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))    
+  }
+  else{
+    return []
+  }
+}
+
 function App() {
   const [name,setName]=useState('');
-  const [list,setList]=useState([]);
+  const [list,setList]=useState(getLocalStorage());
   const [isEditing,setIsEditing]=useState(false);
   const [editID,setEditID]=useState(null);
   const [alert,setAlert]=useState({
@@ -19,7 +29,16 @@ function App() {
       showAlert(true,'danger','please enter text')
     }
     else if(name && isEditing){
-      //edit
+      setList(list.map((item)=>{
+        if(item.id===editID){
+          return {...item,title:name}
+        }
+        return item
+      }))
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+      showAlert(true,'success','value changed')
     }
     else{
       //show alert
@@ -39,10 +58,19 @@ function App() {
     showAlert(true,'danger','item deleted')
     setList(list.filter((item)=>item.id!==id))
   }
+  const editItem=(id)=>{
+    const specificItem=list.find((item)=>item.id===id)
+    setIsEditing(true);
+    setEditID(id)
+    setName(specificItem.title)
+  }
 
   const showAlert=(show=false,type='',msg='')=>{
     setAlert({show,type,msg})
   }
+  useEffect(()=>{
+    localStorage.setItem('list',JSON.stringify(list))
+  },[list])
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -58,7 +86,7 @@ function App() {
       </form>
       {list.length>0&&
       <div className="grocery-container">
-      <List items={list} removeItem={removeItem}/>
+      <List items={list} removeItem={removeItem} editItem={editItem}/>
       <button className='clear-btn'onClick={clearList}>clear items</button>
     </div>
       }
--- src/List.js
+++ src/List.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items,removeItem}) => {
+const List = ({items,removeItem,editItem}) => {
   return <div className='grocery-list'>
     {items.map((item)=>{
       const {id,title}=item;
@@ -8,7 +8,8 @@ const List = ({items,removeItem}) => {
         <p className='title'>
           {title}
           <div className="btn-container">
-            <button type='button' className='edit-btn'>
+            <button type='button' className='edit-btn' 
+            onClick={()=>editItem(id)}>
               <FaEdit/>
             </button>
              <button type='button' className='delete-btn' onClick={()=>removeItem(id)}>
